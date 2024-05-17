888aef0f7570908e2136c33326691be207a5b7b1
TonyKun <tal96047@gmail.com>
2024-05-17 13:49:01 +0300

    create the choose applications

--- c-sharp-apps-Tal-Tony-Gerbi/System/ProcessManager.cs
+++ c-sharp-apps-Tal-Tony-Gerbi/System/ProcessManager.cs
@@ -11,9 +11,38 @@ namespace c_sharp_apps_Tal_Tony_Gerbi.System
 {
     internal class ProcessManager
     {
+        public static int print=1;
         public static void MainProcess()
         {
 
+            while(print != 0)
+            {
+                Console.WriteLine("Hi choose your Application u want to use \n" +
+                "1 – Bank App | 2 – Sport App | 3 – Transportation App | 4 – Draft App | 0- Exit");
+
+                print = int.Parse(Console.ReadLine());
+                switch (print)
+                {
+                    case 0:
+                        Console.WriteLine("Bye");
+                        break;
+                    case 1:
+                        BankAppMain.MainEntry();
+                        break;
+                    case 2:
+                        SportAppMain.MainEntry();
+                        break;
+                    case 3:
+                        TransportationAppMain.MainEntry();
+                        break;
+                    case 4:
+                        DraftAppMain.MainEntry();
+                        break;
+                        default:
+                        Console.WriteLine("i dont have any Applications in this number u choose try again");
+                        break ;
+                }
+            }
         }
     }
 }
