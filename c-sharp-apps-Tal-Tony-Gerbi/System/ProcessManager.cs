ec42064e6c6806d8f073df46de054f164b2f1516
TonyKun <tal96047@gmail.com>
2024-05-17 13:29:33 +0300

    Create folders of applications

---
+++ c-sharp-apps-Tal-Tony-Gerbi/System/ProcessManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c_sharp_apps_Tal_Tony_Gerbi.SportApp;
+using c_sharp_apps_Tal_Tony_Gerbi.DraftApp;
+using c_sharp_apps_Tal_Tony_Gerbi.BankApp;
+using c_sharp_apps_Tal_Tony_Gerbi.TransportationApp;
+namespace c_sharp_apps_Tal_Tony_Gerbi.System
+{
+    internal class ProcessManager
+    {
+        public static void MainProcess()
+        {
+
+        }
+    }
+}
