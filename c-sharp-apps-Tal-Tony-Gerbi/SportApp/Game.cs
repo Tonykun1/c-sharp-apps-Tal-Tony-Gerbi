468d26ff663e7a92b8c34cd38ae93d9714ccf54b
TonyKun <tal96047@gmail.com>
2024-06-07 16:39:14 +0300

    I did  score goal  and finish game

--- c-sharp-apps-Tal-Tony-Gerbi/SportApp/Game.cs
+++ c-sharp-apps-Tal-Tony-Gerbi/SportApp/Game.cs
@@ -13,7 +13,7 @@ namespace c_sharp_apps_Tal_Tony_Gerbi.SportApp
         private int goalTeam1;
         private int goalTeam2;
         private bool gameIsActive;
-        private string time=null;
+        private string time= null;
         public Game(Team team1, Team team2, int goalTeam1, int goalTeam2, bool gameIsActive, string time)
         {
             this.team1 = team1;
@@ -22,6 +22,53 @@ namespace c_sharp_apps_Tal_Tony_Gerbi.SportApp
             this.goalTeam2 = 0;
             this.time = time;
             this.gameIsActive = true;
+        }
+        public void ScoreGoal(Team team)
+        {
+            if (this.gameIsActive)
+            {
+                if (team == this.team1)
+                {
+                    this.goalTeam1++;
+                }
+                else if (team == this.team2)
+                {
+                    this.goalTeam2++;
+                }
+                Console.WriteLine($"{team.GetName()} scored a goal!");
+            }
+            else
+            {
+                Console.WriteLine("The game has already finished. No more goals can be scored.");
+            }
+        }
+
+        public void FinishGame()
+        {
+            this.gameIsActive = false;
+
+            Console.WriteLine($"The game has ended. Final scores:");
+            Console.WriteLine($"{team1.GetName()}: {goalTeam1}");
+            Console.WriteLine($"{team2.GetName()}: {goalTeam2}");
+
+            if (this.goalTeam1 > this.goalTeam2)
+            {
+                Console.WriteLine($"The winner is {team1.GetName()}");
+                team1.setPoints(3);
+                team2.setPoints(0);
+            }
+            else if (this.goalTeam2 > this.goalTeam1)
+            {
+                Console.WriteLine($"The winner is {team2.GetName()}");
+                team1.setPoints(0);
+                team2.setPoints(3);
+            }
+            else
+            {
+                Console.WriteLine("The game ended in a draw.");
+                team1.setPoints(1);
+                team2.setPoints(1);
+            }
         }
     }
 }
--- c-sharp-apps-Tal-Tony-Gerbi/SportApp/TestSportApp.cs
+++ c-sharp-apps-Tal-Tony-Gerbi/SportApp/TestSportApp.cs
@@ -26,94 +26,85 @@ namespace c_sharp_apps_Tal_Tony_Gerbi.SportApp
         public static Season[] CreateChampionsLeagueMock()
         {
             Season[] groups = new Season[8];
+            //יצריה של הקבוצה כל אחת בפנרד
             //קבוצה א
             Team bayern = new Team("Bayern", "Bayern");
             Team copenhagen = new Team("Copenhagen", "Copenhagen");
             Team galastray = new Team("Galastray", "Galastray");
             Team manUnited = new Team("Man United", "Manchester");
-            //יצרה של הקבוצות
-            Team[] teams1 = { bayern, copenhagen, galastray, manUnited };
-            //יצירה בתוך עונה
-            Season groupA = new Season(2024, "soccer", "Champoins - Group A", teams1);
-            //הכנסת העונה למערך
-            groups[0] = groupA;
             //קבוצה ב
             Team arsenal = new Team("Arsenal", "London");
             Team psv = new Team("psv", "eindhoven");
             Team lens = new Team("Lens", "Lens");
             Team sevilla = new Team("Sevilla", "Sevilla");
-            //יצרה של הקבוצות
-            Team[] teams2 = { arsenal, psv, lens, sevilla };
-            //יצירה בתוך עונה של הקבוצות
-            Season groupB = new Season(2024, "soccer", "Champoins - Group B", teams2);
-            //הכנסת העונה למערך של העונות
-            groups[1] = groupB;
             //קבוצה ג
             Team realMadrid = new Team(" Real Madrid", "madrid");
             Team napoli = new Team("Napoli", "Napoli");
             Team braja = new Team("Braja", "Braja");
             Team unionBerlin = new Team("Union Berlin", "Berlin");
-            //יצרה של הקבוצות
-            Team[] teams3 = { realMadrid, napoli, braja, unionBerlin };
-            //יצירה בתוך עונה
-            Season groupC = new Season(2024, "soccer", "Champoins - Group C", teams3);
-            //הכנסת העונה למערך
-            groups[2] = groupC;
             //קבוצה ד
             Team realSociedad = new Team("Real Sociedad", "Sebastián");
             Team inter = new Team("Inter", "Milan");
             Team benfica = new Team("benfica", "lisbon");
             Team sazlburg = new Team("salzburg", "salzburg");
-            //יצרה של הקבוצות
-            Team[] teams4 = { realSociedad, inter, benfica, sazlburg };
-            //יצירה בתוך עונה
-            Season groupD = new Season(2024, "soccer", "Champoins - Group D", teams4);
-            //הכנסת העונה למערך
-            groups[3] = groupD;
             //קבוצה ה
             Team atleteco = new Team("Atletico Madrid", "Madrid");
             Team lazio = new Team("Lazio", "Roma");
             Team feyenoord = new Team("Feyenoord", "Rotterdam");
             Team celtic = new Team("Celtic", "Glasgow");
-            //יצרה של הקבוצות
-            Team[] team5 = { atleteco, lazio, feyenoord, celtic };
-            //יצירה בתוך עונה
-            Season groupE = new Season(2024, "soccer", "Champoins - Group E", team5);
-            //הכנסת העונה למערך
-            groups[4] = groupE;
             //קבוצה ו
             Team dortmund = new Team("Dortmund ", "Dortmund");
             Team psg = new Team("PSG ", "Paris");
             Team aCMilan = new Team("AC Milan ", "Milan");
             Team newCastle = new Team("NewCastle ", "United Kingdom");
-            //יצרה של הקבוצות
-            Team[] teams6 = { dortmund, psg, aCMilan, newCastle };
-            //יצירה בתוך עונה
-            Season groupF = new Season(2024, "soccer", "Champoins - Group F", teams6);
-            //הכנסת העונה למערך
-            groups[5] = groupF;
             //קבוצה ז
             Team manchesterCity = new Team("Manchester City ", "Manchester");
             Team rbLeipzig = new Team("RB Leipzig", "leipzig");
             Team youngBoys = new Team("Young Boys", "Bern");
             Team crvana = new Team("Crvena zvezda", "Belgrad");
-            //יצרה של הקבוצות
-            Team[] teams7 = { manchesterCity, rbLeipzig, youngBoys, crvana };
-            //יצירה בתוך עונה
-            Season groupG = new Season(2024, "soccer", "Champoins - Group G", teams7);
-            //הכנסת העונה למערך
-            groups[6] = groupG;
             //קבוצה ח
             Team barcelona = new Team("Barcelona", "Barcelona");
             Team porto = new Team("Porto", "Porto");
             Team shakhtarDonetsk = new Team("Shakhtar Donetsk", "Donetsk");
             Team antwerpFc = new Team("AntwerpFc", "Antwerp");
             //יצרה של הקבוצות
+            Team[] teams1 = { bayern, copenhagen, galastray, manUnited };
+            Team[] teams2 = { arsenal, psv, lens, sevilla };
+            Team[] teams3 = { realMadrid, napoli, braja, unionBerlin };
+            Team[] teams4 = { realSociedad, inter, benfica, sazlburg };
+            Team[] teams5 = { atleteco, lazio, feyenoord, celtic };
+            Team[] teams6 = { dortmund, psg, aCMilan, newCastle };
+            Team[] teams7 = { manchesterCity, rbLeipzig, youngBoys, crvana };
             Team[] teams8 = { barcelona, porto, shakhtarDonetsk, antwerpFc };
-            //יצירה בתוך עונה
+            //יצירה בתוך עונה של הקבוצות
+            Season groupA = new Season(2024, "soccer", "Champoins - Group A", teams1);
+            Season groupB = new Season(2024, "soccer", "Champoins - Group B", teams2);
+            Season groupC = new Season(2024, "soccer", "Champoins - Group C", teams3);
+            Season groupD = new Season(2024, "soccer", "Champoins - Group D", teams4);
+            Season groupE = new Season(2024, "soccer", "Champoins - Group E", teams5);
+            Season groupF = new Season(2024, "soccer", "Champoins - Group F", teams6);
+            Season groupG = new Season(2024, "soccer", "Champoins - Group G", teams7);
             Season groupH = new Season(2024, "soccer", "Champoins - Group H", teams8);
-            //הכנסת העונה למערך
+
+
+            //הכנסת העונה למערך של העונות
+            groups[0] = groupA;
+            groups[1] = groupB;
+            groups[2] = groupC;
+            groups[3] = groupD;
+            groups[4] = groupE;
+            groups[5] = groupF;
+            groups[6] = groupG;
             groups[7] = groupH;
+
+            //הפעלה משחק בין הקבוצות
+            Game firstGame = new Game(bayern, copenhagen, 0, 0, true, "00:00");
+            //גול ראשון לקבוצה א
+            firstGame.ScoreGoal(bayern);
+            //גול ראשון לקבוצה ב
+            firstGame.ScoreGoal(copenhagen);
+            //סיום בתיקו 
+            firstGame.FinishGame();
             return groups;
 
 
