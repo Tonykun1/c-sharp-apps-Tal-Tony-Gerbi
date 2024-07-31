6785aaf65cf78340d6dfa97d8f96658829fa1c2e
TonyKun <tal96047@gmail.com>
2024-07-31 18:33:34 +0300

    add

---
+++ c-sharp-apps-Tal-Tony-Gerbi/TransportationApp/Exam3/Driver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_Tal_Tony_Gerbi.TransportationApp.Exam3
+{
+    public class Driver
+    {
+        private string firstName;
+        private string lastName;
+        private string id;
+        private CargoType type;
+       /*public  bool Approve(CargoType cargoType)
+        {
+            Console.WriteLine(cargoType+" "+" ");
+            return true;
+        }*/
+    }
+}
---
+++ c-sharp-apps-Tal-Tony-Gerbi/TransportationApp/Exam3/IPortable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_Tal_Tony_Gerbi.TransportationApp.Exam3
+{
+    public interface IPortable
+    {
+        public double GetArea();
+        //width, length, height 
+        public int[] GetSize();
+        public int GetVolume();
+
+
+    }
+}
